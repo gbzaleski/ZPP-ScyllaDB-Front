69d7225a91d0954dc85e20897e24cdf67c3ed38e
dfilimonow <sniperimperial@gmail.com>
2021-10-23 14:16:20 +0200

    Added basic template for terminal app

---
+++ frontend/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(
+    <div>
+        <App/>
+    </div>,
+    document.getElementById("root")
+);
