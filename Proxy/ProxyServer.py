329e3bb3de73b2cd372aca7bac00c688001010b5
daniel <dan.filimonow@gmail.com>
2021-11-19 07:46:25 +0100

    Added Proxy Server

--- Proxy/ProxyServer.py
+++ Proxy/ProxyServer.py
@@ -1,30 +1,36 @@
 import websockets
 import asyncio
-
+import socket
 
 class ProxyServer:
-    def __init__(self, websocket_port, address):
-        # Creating websocket
+    def __init__(self, websocket_port, websocket_address, socket_port, socket_address):
         self.web_port = websocket_port
-        self.address = address
-
-    async def run(self):
-        async for websocket in websockets.connect(self.address + ":" + str(self.web_port)):
+        self.web_address = websocket_address
+        self.socket_info = (socket_address, socket_port)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+          
+    def run(self):
+        async def echo(websocket, path):
             try:
-                message_to_send = ''
-                while message_to_send != 'stop':
-                    message_to_send = input()
-                    if message_to_send != 'stop':
-                        print("Sending: ", message_to_send)
-                        await websocket.send(message_to_send)
-                        response = await websocket.recv()
-                        print(response)
-                break
+                self.socket.connect(self.socket_info)
+                async for message in websocket:
+                    try: 
+                        self.socket.sendall('SELECT * FROM table_name;'.encode())
+                        data = self.socket.recv(1)
+                        result = b''
+                        while(len(data) > 0):
+                            result = result + data
+                            data = self.socket.recv(1)
+                        await websocket.send(str(result))
+                    finally:
+                        self.socket.close()
+            except websockets.exceptions.ConnectionClosed as e:
+                print("A client just disconnected")
 
-            except websockets.ConnectionClosed:
-                print("Connection closed")
-                continue
+        start_server = websockets.serve(echo, self.web_address, self.web_port)
 
-
-server = ProxyServer(8222, "ws://localhost")
-asyncio.run(server.run())
+        asyncio.get_event_loop().run_until_complete(start_server)
+        asyncio.get_event_loop().run_forever()
+          
+server = ProxyServer(8222, "localhost", 9042, "localhost")
+server.run()
