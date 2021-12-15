e02890e11ece6b4e167781ba903b49c2c053b2c3
dfilimonow <sniperimperial@gmail.com>
2021-12-15 15:35:19 +0100

    Added conversion tests

--- frontend/src/CQL-Driver/src/utils/conversions.ts
+++ frontend/src/CQL-Driver/src/utils/conversions.ts
@@ -1,31 +1,31 @@
 import { Buffer } from 'buffer';
 import {Byte, Int, Long, Short, Uuid} from "./types";
 
-export const numberToUuid = (value : number) : Uuid => {
-    return {uuid: numberToBuffer(value, 16)}
+export const numberToUuid = (value : bigint) : Uuid => {
+    return {uuid: bigIntToBuffer(value, 16)}
 }
 
-export const numberToLong = (value : number) : Long => {
-    return {long: numberToBuffer(value, 8)}
+export const numberToLong = (value: bigint) : Long => {
+    return {long: bigIntToBuffer(value, 8)}
 }
 
-export const numberToInt = (value : number) : Int => {
-    return {int: numberToBuffer(value, 4)}
+export const numberToInt = (value : bigint) : Int => {
+    return {int: bigIntToBuffer(value, 4)}
 }
 
-export const numberToShort = (value : number) : Short => {
-    return {short: numberToBuffer(value, 2)}
+export const numberToShort = (value : bigint) : Short => {
+    return {short: bigIntToBuffer(value, 2)}
 }
 
-export const numberToByte = (value : number) : Byte => {
-    return {byte: numberToBuffer(value, 1)}
+export const numberToByte = (value : bigint) : Byte => {
+    return {byte: bigIntToBuffer(value, 1)}
 }
 
-const numberToBuffer = (value : number, size : number) : Buffer => {
+const bigIntToBuffer = (value : bigint, size : number) : Buffer => {
     let buf = Buffer.alloc(size);
     for (let i = size - 1; i >= 0; --i) {
-        buf[i] = value & 0xff;
-        value >>= 1;
+        buf[i] = Number(value & BigInt(0xff));
+        value >>= 8n;
     }
     return buf;
 }
