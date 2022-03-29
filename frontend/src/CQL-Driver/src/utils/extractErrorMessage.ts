e051ad78b2a64b4049778c451bc9b47d2d3e30f7
daniel <dan.filimonow@gmail.com>
2022-03-29 19:11:00 +0200

    added most error types

--- frontend/src/CQL-Driver/src/utils/extractErrorMessage.ts
+++ frontend/src/CQL-Driver/src/utils/extractErrorMessage.ts
@@ -1,4 +1,6 @@
-import { bufferToInt, bufferToString } from "./conversions";
+import { getConstantValue } from "typescript";
+import getConsistency from "../functions/Consistency";
+import { bufferToInt, bufferToShort, bufferToShortBytes, bufferToString } from "./conversions";
 const format = require("biguint-format");
 
 const getServerErrorMessage = (errorBody : Buffer) : [string, string] => {
@@ -6,9 +8,77 @@ const getServerErrorMessage = (errorBody : Buffer) : [string, string] => {
 }
 
 const getProtocolErrorMessage = (errorBody : Buffer) : [string, string] => {
-    console.log(errorBody)
     return [bufferToString(errorBody).string.toString(), "Protocol Error"]
 }
+
+const getAuthenticationErrorMessage = (errorBody : Buffer) : [string, string] => {
+    return [bufferToString(errorBody).string.toString(), "Autentication Error"]
+}
+
+
+const getUnavialableExceptionMessage = (errorBody: Buffer): [string, string] => {
+    const consistency = bufferToShort(errorBody).short
+    const required = bufferToInt(errorBody.slice(2)).int
+    const alive = bufferToInt(errorBody.slice(6)).int
+
+    const message = 
+        "Consistency of query that triggered exception: " +
+        format(consistency) + ". " + 
+        "Required nodes alive: " +
+        format(required) + ", " + 
+        "Current nodes alive: " +
+        format(alive) + "."
+
+    return [message  , "Unavailable"]
+}
+
+const getOverloadedMessage = (errorBody: Buffer): [string, string] => {
+    return [bufferToString(errorBody).string.toString(), "Overloaded"]
+}
+
+const getIsBootstrappingMessage = (errorBody: Buffer): [string, string] => {
+    return [bufferToString(errorBody).string.toString(), "Is Bootstrapping"]
+}
+
+const getTruncateErrorMessage = (errorBody: Buffer): [string, string] => {
+    return [bufferToString(errorBody).string.toString(), "Truncate Error"]
+}
+
+const getSyntaxErrorMessage = (errorBody: Buffer): [string, string] => {
+    return [bufferToString(errorBody).string.toString(), "Syntax Error"]
+}
+
+const getUnautorizedMessage = (errorBody: Buffer): [string, string] => {
+    return [bufferToString(errorBody).string.toString(), "Unauthorized"]
+}
+
+const getInvalidMessage = (errorBody: Buffer): [string, string] => {
+    return [bufferToString(errorBody).string.toString(), "Invalid"]
+}
+
+const getConfigErrorMessage = (errorBody: Buffer): [string, string] => {
+    return [bufferToString(errorBody).string.toString(), "Config Error"]
+}
+
+const getAlreadyExistsMessage = (errorBody: Buffer): [string, string] => {
+    const keyspace = bufferToString(errorBody).string
+    const table = bufferToString(errorBody.slice(keyspace.length)).string
+
+    const message = table.length == 0 ? 
+        "Keyspace " + keyspace.toString() + " already exists." :
+        "Table " + keyspace.toString() + "/" + table.toString() + "already exists."
+
+    throw [message, "Already Exists"];
+}
+
+const getUnpreparedMessage = (errorBody: Buffer): [string, string] => {
+    const id = BigInt(format(bufferToShortBytes(errorBody).shortBytes))
+    const message = 
+        "Prepared statement of ID: " + id.toString() + " does not exist."
+    return [message, "Unprepared"]
+}
+
+
 
 const extractErrorMessage = (messageBody : Buffer) : [string, string] => {
     const errorCode = Number(format(bufferToInt(messageBody).int))
@@ -16,11 +86,47 @@ const extractErrorMessage = (messageBody : Buffer) : [string, string] => {
     messageBody = messageBody.slice(4)
     
     switch (errorCode) {
-        case 0: {
+        case 0x000: {
             return getServerErrorMessage(messageBody);
         }
-        case 10: {
+        case 0x00A: {
             return getProtocolErrorMessage(messageBody);
+        }
+        case 0x0100: {
+            return getAuthenticationErrorMessage(messageBody);
+        }
+        case 0x1000: {
+            return getUnavialableExceptionMessage(messageBody);
+        }
+        case 0x1001: {
+            return getUnavialableExceptionMessage(messageBody);
+        }
+        case 0x1001: {
+            return getOverloadedMessage(messageBody);
+        }
+        case 0x1002: {
+            return getIsBootstrappingMessage(messageBody);
+        }
+        case 0x1003: {
+            return getTruncateErrorMessage(messageBody);
+        }
+        case 0x2000: {
+            return getSyntaxErrorMessage(messageBody);
+        }
+        case 0x2100: {
+            return getUnautorizedMessage(messageBody);
+        }
+        case 0x2200: {
+            return getInvalidMessage(messageBody);
+        }
+        case 0x2300: {
+            return getConfigErrorMessage(messageBody);
+        }
+        case 0x2400: {
+            return getAlreadyExistsMessage(messageBody);
+        }
+        case 0x2500: {
+            return getUnpreparedMessage(messageBody);
         }
     }
 
